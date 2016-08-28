7eb1dff4a85c4267993b91a7e990e50b6056ed25
Crispin Wellington <retrogradeorbit@gmail.com>
2016-08-28 22:41:54 +0800

    nighttime fragment shader

--- src/caveman/core.cljs
+++ src/caveman/core.cljs
@@ -260,6 +260,37 @@
     s)
 )
 
+(def fragment-shader-glsl
+
+  "
+precision mediump float;
+varying vec4 vColor;
+varying vec2 vTextureCoord;
+uniform sampler2D u_texture; //diffuse map
+uniform sampler2D u_lightmap;   //light map
+uniform vec2 resolution; //resolution of screen
+uniform vec4 ambientColor; //ambient RGB, alpha channel is intensity
+uniform float alpha;
+uniform float fire;
+void main() {
+    // the lavers base piel colour
+    vec4 diffuseColor = texture2D(u_texture, vTextureCoord);
+
+    // the light at this point's colour
+    vec2 lighCoord = (gl_FragCoord.xy / resolution.xy);
+    vec4 light = texture2D(u_lightmap, vTextureCoord);
+    float intensity = light.r * fire;
+    vec3 factor=vec3(intensity, intensity, intensity);
+    vec3 avec = vec3(alpha, alpha, alpha);
+    vec3 inv_avec = vec3(1.0-alpha, 1.0-alpha, 1.0-alpha);
+
+    vec3 finalColor = diffuseColor.rgb * factor * vec3(alpha, alpha, alpha)
+                    + diffuseColor.rgb * inv_avec;
+    gl_FragColor = vec4(finalColor, diffuseColor.a); // + vec4(finalColor, diffuseColor.a * alpha);
+}
+"
+
+)
 (defonce main
   (go
     (<! (r/load-resources canvas :ui ["img/sprites.png"
